1ad3d8320741972c9b456859f33e6f3f23474634
oktayudha05 <oktayudha05@gmail.com>
2024-05-13 01:14:57 +0700

    add sign in and styling home

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import Header from "./header";
-
+import Header from "../components/Header";
+import ContentImage from "../components/ContentImage";
+import ContentAction from "../components/ContentAction";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 className="text-6xl font-bold">Hello World</h1>
-      </main>
+      <ContentImage />
+      <ContentAction />
     </>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,11 @@
 /* eslint-disable react/no-unescaped-entities */
+'use client'
+import Link from "next/link";
+
 export default function Header() {
     return (
         <header>
-        <div className="mx-auto max-w-screen-xl px-4 py-4 sm:px-6 sm:py-6 lg:px-8">
+        <div className="mx-auto max-w-screen-xl px-4 py-4 sm:px-4 sm:py-6 lg:px-6">
             <div className="sm:flex sm:items-center sm:justify-between">
             <div className="text-center sm:text-left">
                 <h1 className="text-2xl font-bold text-indigo-400 sm:text-3xl">Welcome!</h1>
@@ -11,16 +14,16 @@ export default function Header() {
             </div>
 
             <div className="mt-4 flex flex-col gap-4 sm:mt-0 sm:flex-row sm:items-center">
-                <button
+                <Link href="/signin"
                 className="inline-flex items-center justify-center gap-1.5 rounded-lg border border-gray-200 px-5 py-3 text-gray-300 transition hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:ring"
                 type="button">
-                <span className="text-sm font-medium"> Signup </span> 
-                </button>
+                <span className="text-sm font-medium"> Sign In </span> 
+                </Link>
 
                 <button
                 className="block rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring"
                 type="button">
-                Signin
+                Sign Up
                 </button>
             </div>
             </div>
